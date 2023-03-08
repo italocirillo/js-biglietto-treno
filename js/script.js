c3bf3aeabbca6768c5e8208d45ca8bd3a5911532
italocirillo <italocirillo@gmail.com>
2023-03-08 17:28:13 +0100

    Controlli sugli input bonus inseriti

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@ let distanza=parseInt(prompt("Ciao sono il controllore del treno,quanti km devi 
 console.log(distanza);
 let età=parseInt(prompt("Puoi dirmi quanti anni hai?(I minorenni hanno il 20% di sconto mentre gli anziani over 65 hanno il 40%)"));
 console.log(età);
+// Controllo che l'utente non abbia scritto dei dati sbagliati
+if(isNaN(distanza)||isNaN(età)){
+    console.log("L'utente non ha scritto un numero");
+    alert("                                         ERRORE\nHAI INSERITO DELLE LETTERE AL POSTO DEI NUMERI,RICARICA E INSERISCI I DATI CORRETTAMENTE");
+}else if(età<=0||età>=150||distanza<=0){
+    console.log("L'utente non ha scritto un età o una distanza fuori dai limiti");
+    alert("                                         ERRORE\nHAI INSERITO UN'ETÀ O UNA DISTANZA NON VALIDA O NON ADATTA PER QUESTO VIAGGIO,RICARICA E INSERISCI I DATI CORRETTAMENTE");
+}
 //Inizializzo le altre variabili
 const prezzoKm=0.21;
 const scontoMinorenni=0.2;
